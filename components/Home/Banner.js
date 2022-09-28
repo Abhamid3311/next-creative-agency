9886ff0692ec09b929fc941e3da9ac596c28115c
Hamid <www.abdulhamid3311@gmail.com>
2022-09-28 11:21:22 +0600

    added prio

--- components/Home/Banner.js
+++ components/Home/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <div className="hero min-h-screen bg-secondary text-primary px-10">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <Image src="/logos/Frame.png" width={825} height={622} className="max-w-sm rounded-lg shadow-2xl" alt='banner-frame' />
+                <Image src="/logos/Frame.png" priority width={825} height={622} className="max-w-sm rounded-lg shadow-2xl" alt='banner-frame' />
                 <div>
                     <h1 className="text-5xl font-bold">Let&apos;s Grow Your <br /> Brand To The <br /> Next Level</h1>
 
